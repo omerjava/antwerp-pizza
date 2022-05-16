159af1cb933018e2d25baeb63f5a2fad3687f283
omer java <87907988+omerjava@users.noreply.github.com>
2022-05-16 23:17:13 +0200

    update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -17,17 +17,17 @@ function Navbar() {
       <div className="leftSide" id={openLinks ? "open" : "close"}>
         <img src={Logo} alt="logo" width="60" length="60" />
         <div className="hiddenLinks">
-          <Link to="/"> Home </Link>
-          <Link to="/menu"> Menu </Link>
-          <Link to="/about"> About </Link>
-          <Link to="/contact"> Contact </Link>
+          <Link to="/antwerp-pizza/"> Home </Link>
+          <Link to="/antwerp-pizza/menu"> Menu </Link>
+          <Link to="/antwerp-pizza/about"> About </Link>
+          <Link to="/antwerp-pizza/contact"> Contact </Link>
         </div>
       </div>
       <div className="rightSide">
-        <Link to="/"> Home </Link>
-        <Link to="/menu"> Menu </Link>
-        <Link to="/about"> About </Link>
-        <Link to="/contact"> Contact </Link>
+        <Link to="/antwerp-pizza/"> Home </Link>
+        <Link to="/antwerp-pizza/menu"> Menu </Link>
+        <Link to="/antwerp-pizza/about"> About </Link>
+        <Link to="/antwerp-pizza/contact"> Contact </Link>
         <button onClick={toggleNavbar}>
           <ReorderIcon />
         </button>
